550614c24696881f1e0ea16aac4ef19cd53838ff
Nightowl <79215414+nightowl286@users.noreply.github.com>
2023-04-09 17:11:17 +0100

    Added member info extensions.

---
+++ src/Common/Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+
+namespace TNO.Common.Extensions;
+
+/// <summary>
+/// Contains useful extension methods related to the <see cref="MemberInfo"/>.
+/// </summary>
+public static class MemberInfoExtensions
+{
+   #region Methods
+   /// <summary>
+   /// Checks whether the given <paramref name="memberInfo"/> has defined 
+   /// an <see cref="Attribute"/> of the type <typeparamref name="T"/>.
+   /// </summary>
+   /// <typeparam name="T">The type of the <see cref="Attribute"/>.</typeparam>
+   /// <param name="memberInfo">The member to check.</param>
+   /// <param name="inherit">
+   /// <see langword="true"/> to search the inheritance chain of the <paramref name="memberInfo"/> to find the attributes,
+   /// <see langword="false"/> otherwise. This parameter is ignored for properties and events.
+   /// </param>
+   /// <returns>
+   /// <see langword="true"/> if one or more instances of the <see cref="Attribute"/> of type <typeparamref name="T"/>
+   /// or any of its derived types is applied to this member, <see langword="false"/> otherwise.
+   /// </returns>
+   public static bool IsDefined<T>(this MemberInfo memberInfo, bool inherit) where T : Attribute
+      => memberInfo.IsDefined(typeof(T), inherit);
+
+   /// <summary>
+   /// Checks whether the given <paramref name="memberInfo"/> has defined
+   /// an <see cref="Attribute"/> of the type <typeparamref name="T"/>.
+   /// </summary>
+   /// <typeparam name="T">The type of the <see cref="Attribute"/>.</typeparam>
+   /// <param name="memberInfo">The member to check.</param>
+   /// <returns>
+   ///  <see langword="true"/> if an <see cref="Attribute"/> of the type <typeparamref name="T"/> 
+   ///  is applied to the given <paramref name="memberInfo"/>, <see langword="false"/> otherwise.
+   /// </returns>
+   public static bool IsDefined<T>(this MemberInfo memberInfo) where T : Attribute
+      => memberInfo.IsDefined(typeof(T));
+   #endregion
+}
